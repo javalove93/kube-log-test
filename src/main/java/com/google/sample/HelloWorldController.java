19c023067c43ee0acd26fa1cec8e9a1d83f26d9a
Jerry Jeong <javalove93@gmail.com>
2018-11-27 02:22:41 +0900

    file logging

--- src/main/java/com/google/sample/HelloWorldController.java
+++ src/main/java/com/google/sample/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.google.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +12,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/sample")
 public class HelloWorldController {
+	private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
+
+	private int count = 0;
+	
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public String isRunning() {
         return "I'm Alive!";
     }
+
+    @RequestMapping(value = "/file", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String fileLogging() {
+    	Thread[] worker = new Thread[100];
+    	
+    	long startTime = System.currentTimeMillis();
+    	
+		count = 0;
+    	for(int i = 0; i < worker.length; i++) {
+    		worker[i] = new Thread() {
+    			public void run() {
+    				for (int i = 0; i < 50000; i++) {
+    					LOG.info("{} : sdklfadljkdsljkfdsljkfdlsjfjlksljfksadjklasjdkl", i);
+    					count++;
+    					
+    					if(count % 10000 == 0) {
+    						System.out.println("Count: " + count);
+    					}
+    				}
+    			}
+    		};
+    		
+    		worker[i].start();
+    	}
+    	
+    	for(int i = 0; i < worker.length; i++) {
+    		try {
+				worker[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	
+    	long endTime = System.currentTimeMillis();
+    	
+        return "Elapsed: " + (endTime - startTime) + "\n";
+    }
+
+
 }
